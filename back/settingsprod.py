6ffdfe29bf61334376880fed889b918023079a69
Kairos6t <kairos6t@gmail.com>
2021-10-19 09:54:16 +0300

    Test config heroku

--- back/settingsprod.py
+++ back/settingsprod.py
@@ -1,10 +1,12 @@
+from back.settings import *
 import dj_database_url
-from .settings import *
 
 
 DEBUG = False
 
 TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['backend-kairos.herokuapp.com']
 
 DATABASES['default'] = dj_database_url.config()
 
@@ -12,4 +14,3 @@ MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-ALLOWED_HOSTS = ['backend-kairos.herokuapp.com']
